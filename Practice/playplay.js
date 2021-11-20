8c0c0fd96584742a0e80c14f253517f0e1f52861
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2021-11-20 13:26:44 +0100

    changed start and dev in package.json file to used nodemon in server runtime

--- Practice/playplay.js
+++ Practice/playplay.js
@@ -14,7 +14,10 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
+    if(req.url === '/'){
+        res.end('<h1>This is going well so far</h1>');
+    }
+    //console.log(req.url);
 });
 
 const PORT = process.env.PORT || 5005;
