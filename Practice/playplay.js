e606c4f8bce254dc5747fdcb73858c5933e201d3
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2021-11-20 13:56:26 +0100

    add writehead

--- Practice/playplay.js
+++ Practice/playplay.js
@@ -15,7 +15,8 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     if(req.url === '/'){
-        res.end('<h1>This is going well so far</h1>');
+        res.writeHead(200, {' Content-Type': 'text/html'});
+        res.end('<h1>how can i change text (edited)</h1>');
     }
     //console.log(req.url);
 });
