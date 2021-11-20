de78b8257069dd1e214c7f3efb75d7a3209e6ba7
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2021-11-20 14:45:25 +0100

    omor, i don tire to dey type but write localhowst:5005/api/users

--- Practice/playplay.js
+++ Practice/playplay.js
@@ -15,11 +15,32 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     if(req.url === '/'){
-        res.writeHead(200, {' Content-Type': 'text/html'});
-        res.end('<h1>how can i change text (edited)</h1>');
+        fs.readFile(path.join(__dirname, 'public', 'index.html'),  
+        (err, content) => {
+                if(err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content);
+        });       
     }
     //console.log(req.url);
+    if(req.url === '/api/users'){
+            const users = [
+                {name: 'Bob Smith', age: 40 },
+                {name: 'John Doe', age: 30 }
+            ]; 
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(users));
+
+        // fs.readFile(path.join(__dirname, 'public', 'about.html'),  
+        // (err, content) => {
+        //         if(err) throw err;
+        //     res.writeHead(200, {'Content-Type': 'text/html'});
+        //     res.end(content);
+        //});       
+    }
 });
+
+
 
 const PORT = process.env.PORT || 5005;
 
